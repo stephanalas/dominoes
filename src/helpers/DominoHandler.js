54d95f553d6e8e76c49955b8481724ab5c0f5afa
stephanalas <72700716+stephanalas@users.noreply.github.com>
2021-11-14 14:19:41 -0500

    created dominoHandler

--- src/helpers/DominoHandler.js
+++ src/helpers/DominoHandler.js
@@ -1,3 +1,70 @@
+import Domino from './dominoes/Domino';
+
 export default class DominoHandler {
-  constructor() {}
+  constructor(scene) {
+    this.createAllDominoes = () => {
+      const min = 0;
+      const max = 6;
+      const dominoes = [];
+
+      let k = 0;
+      for (let i = min; i <= max; i++) {
+        // min = 0 || i = 0
+        for (let j = k; j <= max; j++) {
+          // j = 0
+          dominoes.push(new Domino(scene, [i, j]));
+          // domino double blank
+        }
+        k++;
+      }
+      let x = 200;
+      let y = 200;
+      dominoes.map((domino, idx) => {
+        if (idx % 7 === 0) {
+          x += 50;
+          y = 200;
+        }
+        // every 6 elements after the first two elements are twin dominoes
+        // 0 != 0, 0
+        if (idx === 0) {
+          domino.render(x, y, 'blanks', idx);
+        }
+        domino.render(x, y, 'player', idx);
+        y += 30;
+      });
+      this.layoutDominoes = (x, y) => {
+        let deltaY = y;
+
+        // groups should work for opponents and teammates
+        for (let i = 1; i <= 4; i++) {
+          scene.add.group({
+            key: 'blanks',
+            frame: 0,
+            repeat: 7,
+            setXY: { x, y: deltaY, stepX: 40 },
+          });
+          deltaY += 50;
+        }
+        // for (let i = 0; i < 4; i++) {
+        //   scene.add.group({
+        //     key: 'blanks',
+        //     frame: 0,
+        //     repeat: 7,
+        //     setXY: { x: 300, deltaY, stepX: 40 },
+        //   });
+        //   deltaY *= i + 1;
+        // }
+        // create dominoes 28 dominoes
+        // assign each dominoes data
+        // maybe use a loop to add k, v pairs to an object to check for repeats
+        // console.log('scene from Domino handler', scene);
+        // const dominoes = scene.add.group();
+        // dominoes.createMultiple(28, 'blanks', [0], true);
+        // return dominoes;
+        // const domino = new Domino(scene, [0, 0]);
+
+        // domino.render(x, y, 0, 0);
+      };
+    };
+  }
 }
