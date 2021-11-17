e537b68b0dbbb5224a13f01de09e533ec8600f85
stephanalas <72700716+stephanalas@users.noreply.github.com>
2021-11-16 22:12:50 -0500

    refactored code

--- src/helpers/DominoHandler.js
+++ src/helpers/DominoHandler.js
@@ -2,7 +2,8 @@ import Domino from './dominoes/Domino';
 import DominoMap from './dominoes/DominoMap';
 export default class DominoHandler {
   constructor(scene) {
-    this.createAllDominoes = () => {
+    this.dominoes = [];
+    this.createDominoes = () => {
       const min = 0;
       const max = 6;
       const dominoes = [];
@@ -19,35 +20,46 @@ export default class DominoHandler {
         }
         k++;
       }
-      let x = 200;
-      let y = 200;
-
-      // const finalDominoes = [];
-      dominoes.forEach((domino, idx) => {
-        if (idx % 7 === 0) {
-          // x += 50;
-          // y = 200;
-          y += 30;
-          x = 200;
-        }
-
-        domino.render(x, y, !idx ? 'blanks' : 'player');
-        x += 50;
-      });
+      this.dominoes = dominoes;
     };
     this.layoutDominoes = (x, y) => {
-      let deltaY = y;
+      // const finalDominoes = [];
+      if (!this.dominoes.length) {
+        throw Error('You must create dominoes');
+      } else {
+        this.dominoes.forEach((domino, idx) => {
+          if (idx % 7 === 0) {
+            // x += 50;
+            // y = 200;
+            y += 30;
+            x = 200;
+          }
 
-      // groups should work for opponents and teammates
-      for (let i = 1; i <= 4; i++) {
-        scene.add.group({
-          key: 'blanks',
-          frame: 0,
-          repeat: 7,
-          setXY: { x, y: deltaY, stepX: 40 },
+          domino.render(x, y, !idx ? 'blanks' : 'player');
+          x += 50;
         });
-        deltaY += 50;
       }
+    };
+    this.shuffleDominoes = () => {
+      function shuffle(array) {
+        var m = array.length,
+          t,
+          i;
+
+        // While there remain elements to shuffle…
+        while (m) {
+          // Pick a remaining element…
+          i = Math.floor(Math.random() * m--);
+
+          // And swap it with the current element.
+          t = array[m];
+          array[m] = array[i];
+          array[i] = t;
+        }
+
+        return array;
+      }
+      this.dominoes = shuffle(this.dominoes);
     };
   }
 }
