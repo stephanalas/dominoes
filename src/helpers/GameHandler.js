3008caf0f37f5569695435838a98e69538c56383
stephanalas <72700716+stephanalas@users.noreply.github.com>
2021-11-16 22:13:05 -0500

    created gamehandler

--- src/helpers/GameHandler.js
+++ src/helpers/GameHandler.js
@@ -1,3 +1,28 @@
+import Game from './dominoes/Game';
 export default class GameHandler {
-  constructor() {}
+  constructor(scene) {
+    this.currentGame = null;
+
+    this.createGame = (type, playerCount) => {
+      if (this.currentGame) {
+        this.endGame();
+      }
+      this.currentGame = new Game({
+        gameType: type,
+        totalPlayers: playerCount,
+      });
+      scene.DominoHandler.layoutDominoes(250, 200);
+      return this.currentGame;
+    };
+    this.startGame = () => {
+      // deal shuffled dominoes
+      // making sure that player dominoes is reveal and bot dominoes aren't
+      // bot automatically playes double six if not current turn is player and wait for draggable input sprite to hit zone
+      // if no double six try double 5 etc
+      // place dominoes in dropzone
+    };
+    this.endGame = () => {
+      this.currentGame = null;
+    };
+  }
 }
