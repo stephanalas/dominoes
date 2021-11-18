dfa837a6662b0814d456e5940e8358868c6c9f0a
stephanalas <72700716+stephanalas@users.noreply.github.com>
2021-11-18 10:55:42 -0500

    added method to set group interactivity

--- src/helpers/InteractivityHandler.js
+++ src/helpers/InteractivityHandler.js
@@ -1,3 +1,10 @@
 export default class InteractiveHandler {
-  constructor() {}
+  constructor(scene) {
+    this.setGroupInteractive = (groupChildren) => {
+      groupChildren.forEach((child) => {
+        child.setInteractive();
+        scene.input.setDraggable(child);
+      });
+    };
+  }
 }
